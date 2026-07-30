abb8868c18206df4f23c7e54a26651d8c2551a57
Gaurav Janghu <24csu063@ncuindia.edu>
2026-07-30 11:37:51 +0530

    Removed search bar for temporary purpose

--- telcomitra-admin/src/pages/knowledge-base/KnowledgeBase.jsx
+++ telcomitra-admin/src/pages/knowledge-base/KnowledgeBase.jsx
@@ -52,14 +52,15 @@ const KnowledgeBase = () => {
   return (
     <div className="flex flex-col gap-5">
       <div className="flex justify-between">
-        <input
+        {/* <input
           type="text"
+
           className="bg-gray-900 w-80 hpy-2 rounded-xl border-2 border-gray-800 text-white placeholder:text-gray-500 p-2 focus:outline-none
 focus:border-green-500
 focus:ring-2
 focus:ring-green-500/20"
           placeholder="Search FAQ ..."
-        />
+        /> */}
         <div className="flex gap-5">
           <button
             className="px-4 py-2
--- telcomitra-admin/src/pages/tickets/Tickets.jsx
+++ telcomitra-admin/src/pages/tickets/Tickets.jsx
@@ -45,14 +45,14 @@ const Tickets = () => {
   return (
     <div className="flex flex-col gap-5">
       <div>
-        <input
+        {/* <input
           type="text"
           className="bg-gray-900 w-80 hpy-2 rounded-xl border-2 border-gray-800 text-white placeholder:text-gray-500 p-2 focus:outline-none
 focus:border-green-500
 focus:ring-2
 focus:ring-green-500/20"
           placeholder="Search Tickets ..."
-        />
+        /> */}
       </div>
       <div className="flex ">
         <TicketsTable
